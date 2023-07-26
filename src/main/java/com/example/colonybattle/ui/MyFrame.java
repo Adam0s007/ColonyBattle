eaeda21840313a7b5f5c262e8cf5112a2e88d11d
Adam Biśta <adamosekk2002@gmail.com>
2023-07-26 15:01:31 +0200

    refactor: move grid functionality to GridPanel

--- src/main/java/com/example/colonybattle/ui/MyFrame.java
+++ src/main/java/com/example/colonybattle/ui/MyFrame.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 public class MyFrame extends JFrame {
     private Cell[][] grid;
-
+    public final GridPanel gridPanel;
 
     public MyFrame(int gridSize) {
         // ... konstruktor ...
@@ -17,78 +17,14 @@ public class MyFrame extends JFrame {
         this.setResizable(false);
         this.setSize(800, 800);
 
-        this.setLayout(new GridLayout(gridSize, gridSize,0,0));
-        grid = new Cell[gridSize][gridSize];
-        for (int i = 0; i < gridSize; i++) {
-            for (int j = 0; j < gridSize; j++) {
-                grid[i][j] = new Cell(i, j);
-                this.add(grid[i][j]);
-            }
-        }
+        this.setLayout(new BorderLayout());
+        gridPanel = new GridPanel(gridSize);
+        this.add(gridPanel, BorderLayout.CENTER);
         this.setVisible(true);
     }
 
     // Metoda do pobierania komórki na danej pozycji
-    public Cell getCellAtPosition(Point2d position) {
-        if(position == null) return null;
-        return grid[position.getX()][position.getY()];
-    }
-    // Metoda do ustawiania koloru komórki na podstawie pozycji
-    public void setColorAtPosition(Point2d position, Color color) {
-        if(position == null) return;
-        grid[position.getX()][position.getY()].updateColors(color);
-    }
-
-
-    public void setInitColor(Point2d position){
-       if(position == null) return;
-        grid[position.getX()][position.getY()].initColor();
-    }
-
-    // Metoda do ustawiania liczby żyć na danej pozycji
-    public void setLifeAtPosition(Point2d position, int life) {
-        if(position == null) return;
-        grid[position.getX()][position.getY()].updateLife(life);
-    }
-    public void  setEnergyAtPosition(Point2d position, int energy){
-        if(position == null) return;
-        grid[position.getX()][position.getY()].updateEnergy(energy);
-    }
-
-    public void setInitial(Point2d position, Character initial){
-        if(position == null) return;
-        grid[position.getX()][position.getY()].updateInitial(initial);
-    }
-
-    // Metoda do ustawiania obrazka na danej pozycji
-    // Metoda do ustawiania obrazka na danej pozycji
-    public void setImageAtPosition(Point2d position, ImageIcon icon) {
-        //System.out.println(icon == null);
-        if(position == null) return;
-        grid[position.getX()][position.getY()].setImageIcon(icon);
-    }
-
-    // Metoda do usuwania obrazka z danej pozycji
-    public void removeImageAtPosition(Point2d position) {
-        if(position == null) return;
-        grid[position.getX()][position.getY()].removeImageIcon();
-    }
-
-    //
-    public void setPositionReferences(Map<String, Point2d> positionReferences){
-        positionReferences.entrySet().stream()
-                .forEach(entry -> {
-                    Point2d position = entry.getValue();
-                    if(position != null)
-                        grid[position.getX()][position.getY()].setPosition(position);
-                });
-    }
-    public void setBackground(Point2d position, Color color){
-       if(position == null) return;
-        grid[position.getX()][position.getY()].setBackground(color);
-    }
-    public Color getColorAtPosition(Point2d position){
-        if(position == null) return null;
-        return grid[position.getX()][position.getY()].getBackground();
-    }
+   public GridPanel getGridPanel(){
+        return this.gridPanel;
+   }
 }
